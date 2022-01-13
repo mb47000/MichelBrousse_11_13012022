c213ad2848455e455542af16424101c79ef8cdff
Brousse <mbweb47@gmail.com>
2022-01-13 14:17:04 +0100

    ✨Implement routing and create matching pages with class component

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import HousingSheet from './pages/HousingSheet'
+import About from './pages/About'
+import Error from './pages/Error'
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>Hello Project 11</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="housing-sheet/:id" element={<HousingSheet />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
-);
-
+)
---
+++ src/pages/HousingSheet/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export class HousingSheet extends Component {
+  render() {
+    return <div>HousingSheet</div>
+  }
+}
+
+export default HousingSheet
