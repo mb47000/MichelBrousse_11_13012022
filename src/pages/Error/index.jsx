25fd6abdf251f0e2fafc9d5b46e0439eba6997fe
Brousse <mbweb47@gmail.com>
2022-01-16 00:40:50 +0100

    🚧 Error page development and integration 💄

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,8 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Error.module.css'
 
-class Error extends Component {
+class Error extends React.Component {
   render() {
-    return <div>404</div>
+    return (
+      <main className="layout">
+        <div className={styles.error}>
+          <h1 className={styles.error__title}>404</h1>
+          <p className={styles.error__message}>
+            Oups! La page que vous demandez n'existe pas.
+          </p>
+          <Link to="/" className={styles.error__link}>
+            Retourner sur la page d’accueil
+          </Link>
+        </div>
+      </main>
+    )
   }
 }
 
